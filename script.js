938d907064b7db31701d031498576fb79a83433d
navneetnavs <navneetkumar2004@icloud.com>
2023-11-15 21:57:28 +0530

    Updated

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const slides = document.querySelectorAll("#main")
-var counter = 0;
-slides.forEach(
-  (slide,index) => {
-    slide.computedStyleMap.left =`${index * 100}%`
-  })
-const goPrev = () => {
-  counter --
-  slideImage()
-}
-const goNext = () => {
-  counter ++
-  slideImage()
-}
-const slideImage = ()=> {
-  slides.forEach(
-    (slide) => {
-      slide.style.transform = `translateX(-${counter*100}%)`
-    }
-  )
-}
+// const slides = document.querySelectorAll("#main")
+// var counter = 0;
+// slides.forEach(
+//   (slide,index) => {
+//     slide.computedStyleMap.left =`${index * 100}%`
+//   })
+// const goPrev = () => {
+//   counter --
+//   slideImage()
+// }
+// const goNext = () => {
+//   counter ++
+//   slideImage()
+// }
+// const slideImage = ()=> {
+//   slides.forEach(
+//     (slide) => {
+//       slide.style.transform = `translateX(-${counter*100}%)`
+//     }
+//   )
+// }
